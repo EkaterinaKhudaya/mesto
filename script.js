e913a8e1b09be5dfcf2b90e80cd4a1cf0ae12318
Ekaterina Khudaya <katia.lobanova2014@yandex.ru>
2020-12-21 15:33:04 +0300

    feat: add submit function

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
 let editData = document.querySelector('.profile__edit-data-button');
-let popUpCloseButton =  document.querySelector('.popup__close-button');
+let popUpCloseButton = document.querySelector('.popup__close-button');
 let popUp = document.querySelector('.popup');
+let formElement = popUp.querySelector('.popup__container')
+let saveButton = popUp.querySelector('.popup__button');
+
+let userName = document.querySelector('.profile__username');
+let userInfo = document.querySelector('.profile__userinfo');
+let popUpUserName = document.querySelector('.popup__item_username');
+let popUpUserInfo = document.querySelector('.popup__item_userinfo');
+
+/* Функция для передачи данных пользователя в popup */
+function getUserData() {
+    popUpUserName.value = userName.textContent;
+    popUpUserInfo.value = userInfo.textContent;
+}
+
+function setUserData () {
+    userName.textContent = popUpUserName.value;
+    userInfo.textContent =  popUpUserInfo.value;
+    closePopUpForm();
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    userName.textContent = popUpUserName.value;
+    userInfo.textContent =  popUpUserInfo.value;
+}
 
 
 function openPopUpForm() {
-     popUp.classList.add('popup_opened');
+    getUserData()
+    popUp.classList.add('popup_opened');
 }
+
 function closePopUpForm() {
-     popUp.classList.remove('popup_opened');
+    popUp.classList.remove('popup_opened');
 }
+
 
 editData.addEventListener('click', openPopUpForm);
 popUpCloseButton.addEventListener('click', closePopUpForm);
+saveButton.addEventListener('click', setUserData)
+formElement.addEventListener('submit', handleFormSubmit);
+
 
